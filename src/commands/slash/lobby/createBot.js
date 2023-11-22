245db12b9e45153d1ab5f9c534a1975d94c3d296
Frost <frostlive94@gmail.com>
2023-11-23 00:11:23 +0100

    fixed botclient being a local var

--- src/commands/slash/lobby/createBot.js
+++ src/commands/slash/lobby/createBot.js
@@ -2,6 +2,8 @@ const { ChatInputCommandInteraction, SlashCommandBuilder, EmbedBuilder, ButtonSt
 const ExtendedClient = require('../../../class/ExtendedClient');
 const config = require('../../../config')
 const { Client, Enums } = require('fnbr')
+const fs = require('fs').promises;
+let BotClient = null;
 
 module.exports = {
     structure: new SlashCommandBuilder()
@@ -20,7 +22,11 @@ module.exports = {
             .addStringOption(option => 
                 option.setName('skin')
                 .setDescription('Skin Name or CID')
-                .setRequired(true))),
+                .setRequired(true)))
+        .addSubcommand(subcommand =>
+            subcommand.setName('status')
+            .setDescription('Check the status of your Lean Lobby Bot')),
+                
     /**
      * @param {ExtendedClient} client 
      * @param {ChatInputCommandInteraction} interaction 
@@ -28,15 +34,15 @@ module.exports = {
     run: async (client, interaction) => {
         const { options, channel } = interaction;
         var authCode = options.getString('authcode');
-        var BotClient = new Client();
-        if (authCode != undefined) {
+        if (authCode != undefined) 
+        {
             if (authCode.includes('?code=')) {
                 authCode = authCode.split('=')[1].split('"')[0];
             } else {
                 authCode = authCode;
             }
             BotClient = new Client({
-                "defaultStatus": "Bot",
+                "defaultStatus": "Lean Bot by FrostChanger.de",
                 "platform": "WIN",
                 "cachePresences": false,
                 "auth": {
@@ -55,11 +61,14 @@ module.exports = {
         const res = new EmbedBuilder()
         .setColor('#4b16ff')
         .setDescription(`${interaction.user} | Creating BOT...`);
-        if (interaction.options.getSubcommand() === "create") {
+        if (interaction.options.getSubcommand() === "create") 
+        {
             const reply = interaction.reply({embeds: [res]})
             try {
                 BotClient.auth.authorizationCode = authCode
                 BotClient.on('ready', async () => {
+                    BotClient.isReady = true;
+                    BotClient.user.displayName = BotClient.user.displayName;
                     res.setDescription(`*${interaction.user} | Started your BOT as \`\`${BotClient.user.displayName}\`\`*`)
                     interaction.editReply({embeds: [res]})
                 })
@@ -124,20 +133,53 @@ module.exports = {
                     interaction.followUp({embeds: [res], ephemeral: true, components: [components]})
                 })
 
-                await BotClient.login();
+                if (authCode) {
+                    try {
+                        await BotClient.login();
+                    } catch (err) {
+                        console.error('Login failed:', err);
+                    }
+                }
             } catch(err) {
                 const failLogin = new EmbedBuilder()
                     .setColor('#ffffff')
                     .setTitle(`${config.emojis.wrong} You entered an invalid or expired authorization code. Support: https://discord.gg/frostchanger`);
                 await interaction.reply({embeds: [failLogin]});
             }
-        } else if (interaction.options.getSubcommand() === "skin") {
+        } 
+        
+        if (interaction.options.getSubcommand() === "skin") {
             var skinId = options.getString('skin');
-            
-            console.log(BotClient)
-            await BotClient.party.me.setOutfit(skinId)
-            res.setDescription(`*${interaction1.user} | set skin to \`\`${skinId}\`\`*`)
-            interaction.reply({embeds: [res], ephemeral: true})
+        
+            if (BotClient && BotClient.isReady) {
+                try {
+                    await BotClient.party.me.setOutfit(skinId);
+                    console.log(`Outfit set to: ${skinId}`);
+                    res.setDescription(`*${interaction.user} | Set skin to \`\`${skinId}\`\`*`);
+                    interaction.reply({embeds: [res], ephemeral: true});
+                } catch (err) {
+                    console.error('Error setting outfit:', err);
+                    res.setDescription(`*Failed to set skin: ${err.message}*`);
+                    interaction.reply({embeds: [res], ephemeral: true});
+                }
+            } else {
+                res.setDescription('*Bot Client is not ready or not defined*');
+                interaction.reply({embeds: [res], ephemeral: true});
+            }
+        }
+        
+        if (interaction.options.getSubcommand() === "status") {
+            const res = new EmbedBuilder().setColor('#4b16ff');
+
+            // Check if BotClient is defined and ready
+            if (BotClient && BotClient.isReady) {
+                const userStatus = BotClient.user ? `Logged in as ${BotClient.user.displayName}` : 'User not set';
+                res.setDescription(`*Bot Status: Ready*\n${userStatus}`);
+            } else {
+                res.setDescription('*Bot Status: Not Ready*');
+            }
+
+            await interaction.reply({ embeds: [res], ephemeral: true });
         }
     }
 };
